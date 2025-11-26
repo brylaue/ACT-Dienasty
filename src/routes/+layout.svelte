eaf87d101fe18328c523af4b6979f267f636ab5f
brylaue <bryan@ikhaya.io>
2025-11-26 11:39:47 -0500

    Updated +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,24 @@
 	import { Nav, Footer } from "$lib/components";
     import { dev } from '$app/environment';
     import { injectAnalytics } from '@vercel/analytics/sveltekit';
- 
+
     injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
+<style>
+	:global(body) {
+		overflow-x: hidden;
+	}
+
+	main {
+		overflow-x: hidden;
+		width: 100%;
+	}
+</style>
+
 <main>
     <Nav /> <!-- adds the nav (small and large) -->
-  
+
     <slot />
 
     <Footer /> <!-- adds the footer -->
