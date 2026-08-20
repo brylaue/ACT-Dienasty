3e4fceb5b55022e5811a54daaa40f63316a44fda
brylaue <bryan@ikhaya.io>
2026-08-20 12:53:26 -0400

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import vercel from "@sveltejs/adapter-vercel";
-import node from "@sveltejs/adapter-node";
 
-const dockerBuild = process.env.DOCKER_BUILD;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: dockerBuild ? node() : vercel({
+    adapter: vercel({
       runtime: 'nodejs22.x', // nodejs20.x hit end-of-life April 2026; Vercel retires EOL runtimes
       regions: ['iad1']
     }),
